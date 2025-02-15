c85705009932ed939087cd50d30373bd4cd05bf8
imon-sheikh <mdimonhossain2017@gmail.com>
2025-02-15 20:52:17 +0600

    react hook form password regex validation added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,18 +1,17 @@
 import { useForm } from "react-hook-form";
 
-const SignUp = () => { 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-      } = useForm() 
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    const onSubmit = data => {
-        console.log(data) 
-    }
-    // console.log(watch("example")) // watch input value by passing the name of it
-      
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  // console.log(watch("example")) // watch input value by passing the name of it
 
   return (
     <div className="hero bg-base-200 min-h-screen">
@@ -26,37 +25,59 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form 
-          onSubmit={handleSubmit(onSubmit)}
-          className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <fieldset className="fieldset">
               <label className="fieldset-label">Name</label>
               <input
                 type="text"
                 className="input"
                 placeholder="name"
-                name="name" 
-                {...register("name", {required: true})} 
+                name="name"
+                {...register("name", { required: true })}
               />
-                {errors.name && <span className="text-red-600 bg-red-200 border-red-900 p-1">This field is required</span>}
+              {errors.name && (
+                <span className="text-red-600 bg-red-200 border-red-900 p-1">
+                  This field is required
+                </span>
+              )}
               <label className="fieldset-label">Email</label>
               <input
                 type="email"
                 name="email"
                 className="input"
                 placeholder="Email"
-                {...register("email", {required: true})}
-              /> 
-               {errors.email && <span className="text-red-600 bg-red-200 border-red-900 p-1">This field is required</span>}
+                {...register("email", { required: true })}
+              />
+              {errors.email && (
+                <span className="text-red-600 bg-red-200 border-red-900 p-1">
+                  This field is required
+                </span>
+              )}
               <label className="fieldset-label">Password</label>
               <input
                 type="password"
                 name="password"
                 className="input"
                 placeholder="Password"
-                {...register("password", {required: true, minLength: 6, maxLength: 20})}
-              /> 
-              {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                })}
+              />
+              {errors.password?.type === "required" && (
+                <p className="text-red-600">Password is required</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-600">Password must be more than 6 characters</p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p className="text-red-600">Password must be less than 20 characters</p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-600">Password must have one Upper, lower, and special character</p>
+              )}
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
