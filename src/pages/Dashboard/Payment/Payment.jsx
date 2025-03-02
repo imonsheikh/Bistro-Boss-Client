7858786ec9f2c49b88b535c4e6158ce3f549ee48
imon-sheikh <mdimonhossain2017@gmail.com>
2025-03-02 11:50:30 +0600

    added sslcommerz for payment method

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,21 +2,39 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
+import { useState } from "react";
+import SSLPayment from "./SSLPayment";
 
 //TODO: add publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
-const Payment = () => {
+const Payment = () => {  
+     const [method, setMethod] = useState('stripe')
+
     return (
         <div>
             <SectionTitle
             heading={'Payment'}
             subHeading={'Please pay to eat'}
             ></SectionTitle> 
-            <div> 
+            
+            <select 
+            onChange={(e) => setMethod(e.target.value)}  
+            className="select font-bold text-lg mb-5"
+            name="" id="">
+                <option value="stripe">Stripe</option>
+                <option value="SSLCommerce">SSL Commerz</option>
+            </select> 
+
+            {
+                method === 'stripe' ?  <div> 
                 <Elements stripe={stripePromise}>
                   <CheckoutForm></CheckoutForm>
                 </Elements>
-            </div>
+            </div> :  
+            <SSLPayment></SSLPayment>
+            }
+
+           
         </div>
     );
 };
